263da92b7b97235dec08d0b1ffd1a5b17dcf0b9a
Yash <yashatkulwar2000@gmail.com>
2022-08-22 20:07:15 +0530

    added binary from figure

--- BinaryTreeMain.java
+++ BinaryTreeMain.java
@@ -7,9 +7,18 @@ public class BinaryTreeMain {
         Node root = null;
         root = bst.insert(root, 56);
         root = bst.insert(root, 30);
-        // 30->56->70->80
         root = bst.insert(root, 70);
+        root = bst.insert(root, 22);
+        root = bst.insert(root, 40);
+        root = bst.insert(root, 60);
+        root = bst.insert(root, 95);
+        root = bst.insert(root, 11);
+        root = bst.insert(root, 65);
+        root = bst.insert(root, 3);
+        root = bst.insert(root, 6);
+        root = bst.insert(root, 16);
+        root = bst.insert(root, 63);
+        root = bst.insert(root, 67);
         bst.print(root);
-
     }
 }
